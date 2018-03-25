ea02ab937a8fb88fb23edaf5bb23a4b2a1290bf4
bruce <huazhongwei@yeah.net>
2018-03-26 07:38:08 +0800

    db

---
+++ app/src/main/java/com/bruce/weather/data/db/DbClasses.kt
@@ -0,0 +1,5 @@
+package com.bruce.weather.data.db
+
+/**
+ * Created by huazh on 2018/3/26.
+ */
---
+++ app/src/main/java/com/bruce/weather/data/db/ForecastDb.kt
@@ -0,0 +1,7 @@
+package com.bruce.weather.data.db
+
+/**
+ * Created by huazh on 2018/3/26.
+ */
+class ForecastDb {
+}
